392d194587ef3f730b6a686dd264758cb0e7ac89
Anjali-Birajdar <anjali.birajdar2005@gmail.com>
2026-07-15 00:33:18 +0530

    Add AIML folder, Numpy folder and numpy_day1 file

---
+++ AIML/Numpy/numpy_day1.py
@@ -0,0 +1,9 @@
+import numpy as np
+student_marks = np.array([82, 75, 91, 68, 88])
+print(student_marks[0])
+print(student_marks[-1])
+print(np.mean(student_marks))
+print(np.median(student_marks))
+print(np.max(student_marks))
+print(np.min(student_marks))
+print(np.size(student_marks))
